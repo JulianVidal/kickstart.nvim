640fbbe3d4a1d7f93f31007e0762a6df4d0f121e
JulianVidal <julividal03@hotmail.com>
2024-07-08 22:41:02 +0100

    Changed some theming

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -2,3 +2,9 @@ vim.o.background = 'dark'
 vim.cmd [[colorscheme gruvbox]]
 
 vim.cmd [[ highlight SignColumn None ]]
+vim.cmd [[ highlight StatusLineNC None ]]
+vim.cmd [[ highlight StatusLine None ]]
+vim.cmd [[ hi CursorLine None ]]
+vim.cmd [[ hi CursorLineNR None ]]
+
+vim.cmd [[ highlight LSPSignatureActiveParameter guifg=#fe8019]]
