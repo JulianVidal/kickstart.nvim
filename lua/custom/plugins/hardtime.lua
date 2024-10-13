fc04c3deb364367d9f5bec8d2aa22e907ae85e58
JulianVidal <julividal03@hotmail.com>
2024-10-13 22:15:52 +0100

    Updated rules for hardtime.lua

--- lua/custom/plugins/hardtime.lua
+++ lua/custom/plugins/hardtime.lua
@@ -2,6 +2,14 @@ return {
   {
     'm4xshen/hardtime.nvim',
     dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
-    opts = {},
+    opts = {
+      disabled_keys = {
+        ['<Up>'] = {},
+        ['<Down>'] = {},
+        ['<Right>'] = {},
+        ['<Left>'] = {},
+      },
+      disable_mouse = false,
+    },
   },
 }
