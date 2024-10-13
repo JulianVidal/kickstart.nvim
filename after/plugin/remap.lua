aacd8ee7e4c3e16d5d340a06eb70fdc004539652
JulianVidal <julividal03@hotmail.com>
2024-10-13 22:16:06 +0100

    Added latex support

--- after/plugin/remap.lua
+++ after/plugin/remap.lua
@@ -1,8 +1,13 @@
 -- vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = 'Enter Ex-mode' })
 
 require('which-key').add {
+  { '<leader>l', group = '[L]atex' },
+}
+
+require('which-key').add {
   { '<leader>y', group = '[Y]eet' },
 }
+
 -- , { '<leader>y_', hidden = true }
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move Block' })
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move Block' })
