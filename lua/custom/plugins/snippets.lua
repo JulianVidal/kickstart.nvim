93d5831099fb18f620d764d524be394be8001e6d
JulianVidal <49546430+spiderpiggy03@users.noreply.github.com>
2024-10-14 20:02:35 +0100

    Added some latex snippets

---
+++ lua/custom/plugins/snippets.lua
@@ -0,0 +1,2 @@
+require('luasnip.loaders.from_lua').lazy_load { paths = { '~/.config/nvim/lua/custom/snippets' } }
+return {}
--- lua/custom/plugins/vimtex.lua
+++ lua/custom/plugins/vimtex.lua
@@ -4,5 +4,20 @@ return {
   init = function()
     -- VimTeX configuration goes here, e.g.
     vim.g.vimtex_view_method = 'sioyek'
+
+    -- Focus back on nvim on macos according to :h vimtex-events
+    if jit.os == 'OSX' then
+      local au_group = vim.api.nvim_create_augroup('vimtex_events', {})
+      -- Focus the terminal after inverse search
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'VimtexEventViewReverse',
+        group = au_group,
+        -- command = 'silent !open -a iTerm',
+        callback = function()
+          print 'RAN'
+          vim.cmd [[silent !open -a iTerm]]
+        end,
+      })
+    end
   end,
 }
