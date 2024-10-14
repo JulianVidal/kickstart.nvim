1c0048f9343883c67f93bfda6f32ac316f2d3759
JulianVidal <julividal03@hotmail.com>
2024-10-14 02:32:50 +0100

    Now uses sioyek in WSL instead of windows

--- lua/custom/plugins/vimtex.lua
+++ lua/custom/plugins/vimtex.lua
@@ -4,7 +4,5 @@ return {
   init = function()
     -- VimTeX configuration goes here, e.g.
     vim.g.vimtex_view_method = 'sioyek'
-    vim.g.vimtex_view_sioyek_exe = 'sioyek.exe'
-    vim.g.vimtex_callback_progpath = 'wsl nvim'
   end,
 }
