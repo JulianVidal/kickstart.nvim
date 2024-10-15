68c2e54586718bc02e8516b9e32992eeabbe304a
JulianVidal <julividal03@hotmail.com>
2024-10-15 15:34:13 +0100

    Added fix for markdown preview

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -4,6 +4,9 @@ return {
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
     ft = { 'markdown' },
     build = function()
+      -- For future reference run this command to get this plugin going:
+      -- :Lazy build markdown-preview.nvim
+      -- I don't know why, but it works.
       vim.fn['mkdp#util#install']()
     end,
   },
