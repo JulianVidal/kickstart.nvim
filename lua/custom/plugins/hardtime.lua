fd65699cfd16f1f156ba3011d1d713d9325b1ecc
JulianVidal <julividal03@hotmail.com>
2025-03-19 16:56:11 +0000

    Removed hardtime.nvim

--- lua/custom/plugins/hardtime.lua
+++ lua/custom/plugins/hardtime.lua
@@ -2,6 +2,7 @@ return {
   {
     'm4xshen/hardtime.nvim',
     dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
+    enabled = false,
     opts = {
       disabled_keys = {
         ['<Up>'] = {},
